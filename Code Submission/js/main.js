186b6ea4a4104a8521d0f38a251c805545222b48
JamesDun2866 <44902009+JamesDun2866@users.noreply.github.com>
2020-04-24 12:38:41 +0100

    Initial

---
+++ Code Submission/js/main.js
@@ -0,0 +1,20 @@
+(function($) {
+   $(window).on('scroll', function() {
+		var h = $('header').height();
+		var y = $(window).scrollTop();
+      var header = $('#main-header');
+
+	   if ((y > h + 30 ) && ($(window).outerWidth() > 768 ) ) {
+	      header.addClass('opaque');
+	   }
+      else {
+         if (y < h + 30) {
+            header.removeClass('opaque');
+         }
+         else {
+            header.addClass('opaque');
+         }
+      }
+
+	});
+})(jQuery);
